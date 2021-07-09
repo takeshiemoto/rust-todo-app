a327ef8e4e68edc6e5d8553c3d4985fcb0d602dc
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2021-07-09 09:35:29 +0900

    feat: add page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
